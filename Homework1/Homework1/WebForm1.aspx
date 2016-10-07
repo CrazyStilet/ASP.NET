ea29ec40c17012af0b12355d21efaba1d0eeb0e9
CrazyStilet <Stilet-2006@yandex.ru>
2016-10-07 17:28:02 +0300

    Homework1

---
+++ Homework1/Homework1/WebForm1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Homework1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Magazine</title>
+</head>
+<body>
+    <style>
+        div.my{
+            display:inline;
+        }
+        div.cart{
+            margin-top:10px;
+        }
+    </style>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox>Список продуктов</asp:TextBox>
+    </div>
+    <div class="my">
+        <asp:ListBox id="lstProducts" 
+           Rows="6"
+           Width="100px"
+           SelectionMode="Multiple" 
+           runat="server">
+
+         <asp:ListItem>Item 1</asp:ListItem>
+         <asp:ListItem>Item 2</asp:ListItem>
+         <asp:ListItem>Item 3</asp:ListItem>
+         <asp:ListItem>Item 4</asp:ListItem>
+         <asp:ListItem>Item 5</asp:ListItem>
+         <asp:ListItem>Item 6</asp:ListItem>
+
+      </asp:ListBox>
+    </div>
+    <div class="my">
+        <asp:Button ID="btnInCart" runat="server" Text="В корзину" OnClick="btnInCart_Click"/>
+        <asp:Button ID="btnAllInCart" runat="server" Text="Все в корзину" OnClick="btnAllInCart_Click" />
+    </div>
+    <div class="cart">
+        <asp:TextBox>Корзина</asp:TextBox>
+    </div>
+    <div class="my">
+        <asp:ListBox id="lstCart" 
+            Rows="6"
+           Width="100px"
+           SelectionMode="Multiple" 
+           runat="server">
+      </asp:ListBox>
+    </div>
+    <div class="my">
+        <asp:Button ID="btnFromCart" runat="server" Text="Убрать из корзины" OnClick="btnFromCart_Click" />
+        <asp:Button ID="btnAllFromCart" runat="server" Text="Убрать все из корзины" OnClick="btnAllFromCart_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Homework1/Homework1/WebForm1.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Homework1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void btnInCart_Click(object sender, EventArgs e)
+        {
+            if (lstProducts.Items!=null)
+            {
+                foreach (ListItem li in lstProducts.Items)
+                {
+                    if (li.Selected == true)
+                    {
+                        lstCart.Items.Add(li);
+                    }
+                }
+                for (int i = 0; i < lstProducts.Items.Count; i++)
+                {
+                    if (lstProducts.Items[i].Selected)
+                    {
+                        lstProducts.Items.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            
+        }
+        protected void btnFromCart_Click(object sender, EventArgs e)
+        {
+            if (lstCart.Items != null)
+            {
+                foreach (ListItem li in lstCart.Items)
+                {
+                    if (li.Selected == true)
+                    {
+                        lstProducts.Items.Add(li);
+                    }
+                }
+                for (int i = 0; i < lstCart.Items.Count; i++)
+                {
+                    if (lstCart.Items[i].Selected)
+                    {
+                        lstCart.Items.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        protected void btnAllInCart_Click(object sender, EventArgs e)
+        {
+            if (lstProducts.Items!=null)
+            {
+                foreach (ListItem li in lstProducts.Items)
+                {
+                    lstCart.Items.Add(li);
+                }
+                lstProducts.Items.Clear();
+            }
+        }
+
+        protected void btnAllFromCart_Click(object sender, EventArgs e)
+        {
+            if (lstCart.Items!=null)
+            {
+                foreach (ListItem li in lstCart.Items)
+                {
+                    lstProducts.Items.Add(li);
+                }
+                lstCart.Items.Clear();
+            }
+        }
+    }
+}
