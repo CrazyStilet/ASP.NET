3d7a8b7915db5ea1c93919189398a9bf46f7a392
CrazyStilet <Stilet-2006@yandex.ru>
2016-10-24 15:58:49 +0300

    Homework2

---
+++ Homework2/Homework2/MainForm.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="Homework2.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TDividend" runat="server" Width="200px" AutoPostBack="true" OnTextChanged="TDividend_TextChanged"></asp:TextBox>
+        <asp:Label ID="LDividend" runat="server" ForeColor="Red"></asp:Label>
+    </div>
+        <br />
+    <div>
+        <asp:TextBox ID="TDivider" runat="server" Width="200px" AutoPostBack="true" OnTextChanged="TDivider_TextChanged"></asp:TextBox>
+        <asp:Label ID="LDivider" runat="server" ForeColor="Red"></asp:Label>
+    </div>
+        <br />
+        <br />
+    <div>
+        <asp:Button ID="btnDivided" runat="server" Text="Разделить" OnClick="btnDivided_Click"/>
+        <asp:Label ID="LAnswer" runat="server"></asp:Label>
+    </div>
+        <br />
+    <div>
+        <asp:BulletedList ID="bltList" runat="server"></asp:BulletedList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Homework2/Homework2/MainForm.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Homework2
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDivided_Click(object sender, EventArgs e)
+        {
+            double divided = 0, divider = 0;
+            bltList.Items.Clear();
+            try
+            {
+                divided = Convert.ToDouble(TDividend.Text);
+            }
+            catch (Exception ex)
+            {
+                LDividend.Text = ex.Message;
+                bltList.Items.Add(ex.Message);
+            }
+
+            try
+            {
+                divider = Convert.ToDouble(TDivider.Text);
+                if (divider == 0)
+                {
+                    throw new Exception("Недопустимое значение");
+                }
+                else
+                {
+                    double res = divided / divider;
+                    LAnswer.Text = res.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                LDivider.Text = ex.Message;
+                bltList.Items.Add(ex.Message);
+            }
+        }
+
+        protected void TDividend_TextChanged(object sender, EventArgs e)
+        {
+            LDividend.Text = "";
+            LAnswer.Text = "";
+        }
+
+        protected void TDivider_TextChanged(object sender, EventArgs e)
+        {
+            LDivider.Text = "";
+            LAnswer.Text = "";
+        }
+    }
+}
